95e7968b2abe55f6710e4651cc0e6df458f083db
acl13 <128934431+acl13@users.noreply.github.com>
2025-08-12 14:02:47 -0400

    get route data from google maps and send to frontend

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const Bookstore = require("../models/bookstore");
 const bookstoreData = require("../../bookstores.json");
 const mongoose = require("mongoose");
+const axios = require("axios");
+require("dotenv").config();
 
 mongoose.connect(
   "mongodb+srv://anneclinebarger:eUqDuVLLH3eUjoGU@cluster0.al86ipn.mongodb.net/clt-bookstore-crawl"
@@ -19,7 +21,73 @@ router.get("/bookstores", (req, res) => {
     });
 });
 
-// TODO: Add routes for authentication
-// TODO: Write tests for all routes
+const apiKey = process.env.API_KEY;
+
+const getRoute = async (initialLocation, waypoints) => {
+  const url = "https://routes.googleapis.com/directions/v2:computeRoutes";
+
+  const body = {
+    origin: {
+      placeId: initialLocation.place_id,
+    },
+    destination: {
+      placeId: initialLocation.place_id,
+    },
+    intermediates: waypoints.map((waypoint) => ({
+      placeId: waypoint.place_id,
+    })),
+    travelMode: "DRIVE",
+    routingPreference: "TRAFFIC_AWARE",
+    optimizeWaypointOrder: true,
+    computeAlternativeRoutes: false,
+    routeModifiers: {
+      avoidTolls: false,
+      avoidHighways: false,
+      avoidFerries: false,
+    },
+    languageCode: "en-US",
+    units: "METRIC",
+  };
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      "X-Goog-Api-Key": apiKey, // use environment variable
+      "X-Goog-FieldMask": "routes",
+    },
+  };
+
+  try {
+    const response = await axios.post(url, body, config);
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Error getting route:",
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
+
+router.post("/routes", async (req, res) => {
+  console.log("Received request body:", req.body);
+  const { initialLocation, waypoints } = req.body;
+
+  if (
+    !initialLocation ||
+    !initialLocation.place_id ||
+    !Array.isArray(waypoints)
+  ) {
+    return res.status(400).json({ error: "Invalid request body" });
+  }
+
+  try {
+    const routeData = await getRoute(initialLocation, waypoints);
+    console.log(routeData);
+    res.json(routeData);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to compute route" });
+  }
+});
 
 module.exports = router;
